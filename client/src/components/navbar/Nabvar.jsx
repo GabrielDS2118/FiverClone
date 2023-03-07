371a055e0085a3df198d337c43205e3382d8fc72
Gabriel Aristizabal <gabrielhotm2020@outlook.com>
2023-03-07 13:23:47 -0500

    2.Frontend Routing

--- client/src/components/navbar/Nabvar.jsx
+++ client/src/components/navbar/Nabvar.jsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import './Nabvar.scss';
 
 const Nabvar = () => {
   const [active, setActive] = useState(false);
   const [open, setOpen] = useState(false);
+
+  const { pathname } = useLocation();
 
   const isActive = () => {
     window.scrollY > 0 ? setActive(true) : setActive(false);
@@ -22,10 +25,13 @@ const Nabvar = () => {
   };
 
   return (
-    <div className={active ? 'navbar active' : 'navbar'}>
+    <div className={active || pathname !== '/' ? 'navbar active' : 'navbar'}>
       <div className="container">
         <div className="logo">
-          <span className="text">Fiverr</span>
+          <Link to="/" className="link">
+            <span className="text">Fiverr</span>
+          </Link>
+
           <span className="dot">.</span>
         </div>
         <div className="links">
@@ -47,14 +53,24 @@ const Nabvar = () => {
                 <div className="options">
                   {currentUser?.isSeller && (
                     <>
-                      <span>Gigs</span>
-                      <span>Add New Gig</span>
+                      <Link className="link" to="/mygigs">
+                        Gigs
+                      </Link>
+                      <Link className="link" to="/add">
+                        Add New Gig
+                      </Link>
                     </>
                   )}
 
-                  <span>Orders</span>
-                  <span>Message</span>
-                  <span>Logout</span>
+                  <Link className="link" to="/orders">
+                    Orders
+                  </Link>
+                  <Link className="link" to="/messages">
+                    Messages
+                  </Link>
+                  <Link className="link" to="/">
+                    Logout
+                  </Link>
                 </div>
               )}
             </div>
@@ -62,15 +78,41 @@ const Nabvar = () => {
         </div>
       </div>
 
-      {active && (
-        <>
-          <hr />
-          <div className="menu">
-            <span>Test</span>
-            <span>Test2</span>
-          </div>
-        </>
-      )}
+      {active ||
+        (pathname !== '/' && (
+          <>
+            <hr />
+            <div className="menu">
+              <Link className="link menuLink" to="/">
+                Graphics & Design
+              </Link>
+              <Link className="link menuLink" to="/">
+                Video & Animation
+              </Link>
+              <Link className="link menuLink" to="/">
+                Writing & Translation
+              </Link>
+              <Link className="link menuLink" to="/">
+                AI Services
+              </Link>
+              <Link className="link menuLink" to="/">
+                Digital Marketing
+              </Link>
+              <Link className="link menuLink" to="/">
+                Music & Audio
+              </Link>
+              <Link className="link menuLink" to="/">
+                Programming & Tech
+              </Link>
+              <Link className="link menuLink" to="/">
+                Business
+              </Link>
+              <Link className="link menuLink" to="/">
+                Lifestyle
+              </Link>
+            </div>
+          </>
+        ))}
     </div>
   );
 };
