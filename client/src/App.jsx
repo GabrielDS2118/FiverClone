e3853d2b215eb3f89242e2a7b81b8f68eabdbd81
Gabriel Aristizabal <gabrielhotm2020@outlook.com>
2023-03-07 12:25:56 -0500

    1.First commit

---
+++ client/src/App.jsx
@@ -0,0 +1,110 @@
+import React from 'react';
+import Nabvar from './components/navbar/Nabvar';
+
+function App() {
+  return (
+    <div>
+      <Nabvar />
+
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero
+        praesentium velit, ipsam, inventore quaerat sed iste provident nihil
+        ipsa veritatis perspiciatis corrupti. Eaque reprehenderit unde autem
+        fugiat totam assumenda! Est.
+      </h1>
+
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero
+        praesentium velit, ipsam, inventore quaerat sed iste provident nihil
+        ipsa veritatis perspiciatis corrupti. Eaque reprehenderit unde autem
+        fugiat totam assumenda! Est.
+      </h1>
+
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero
+        praesentium velit, ipsam, inventore quaerat sed iste provident nihil
+        ipsa veritatis perspiciatis corrupti. Eaque reprehenderit unde autem
+        fugiat totam assumenda! Est.
+      </h1>
+
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero
+        praesentium velit, ipsam, inventore quaerat sed iste provident nihil
+        ipsa veritatis perspiciatis corrupti. Eaque reprehenderit unde autem
+        fugiat totam assumenda! Est.
+      </h1>
+
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero
+        praesentium velit, ipsam, inventore quaerat sed iste provident nihil
+        ipsa veritatis perspiciatis corrupti. Eaque reprehenderit unde autem
+        fugiat totam assumenda! Est.
+      </h1>
+
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero
+        praesentium velit, ipsam, inventore quaerat sed iste provident nihil
+        ipsa veritatis perspiciatis corrupti. Eaque reprehenderit unde autem
+        fugiat totam assumenda! Est.
+      </h1>
+
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero
+        praesentium velit, ipsam, inventore quaerat sed iste provident nihil
+        ipsa veritatis perspiciatis corrupti. Eaque reprehenderit unde autem
+        fugiat totam assumenda! Est.
+      </h1>
+
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero
+        praesentium velit, ipsam, inventore quaerat sed iste provident nihil
+        ipsa veritatis perspiciatis corrupti. Eaque reprehenderit unde autem
+        fugiat totam assumenda! Est.
+      </h1>
+
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero
+        praesentium velit, ipsam, inventore quaerat sed iste provident nihil
+        ipsa veritatis perspiciatis corrupti. Eaque reprehenderit unde autem
+        fugiat totam assumenda! Est.
+      </h1>
+
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero
+        praesentium velit, ipsam, inventore quaerat sed iste provident nihil
+        ipsa veritatis perspiciatis corrupti. Eaque reprehenderit unde autem
+        fugiat totam assumenda! Est.
+      </h1>
+
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero
+        praesentium velit, ipsam, inventore quaerat sed iste provident nihil
+        ipsa veritatis perspiciatis corrupti. Eaque reprehenderit unde autem
+        fugiat totam assumenda! Est.
+      </h1>
+
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero
+        praesentium velit, ipsam, inventore quaerat sed iste provident nihil
+        ipsa veritatis perspiciatis corrupti. Eaque reprehenderit unde autem
+        fugiat totam assumenda! Est.
+      </h1>
+
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero
+        praesentium velit, ipsam, inventore quaerat sed iste provident nihil
+        ipsa veritatis perspiciatis corrupti. Eaque reprehenderit unde autem
+        fugiat totam assumenda! Est.
+      </h1>
+
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero
+        praesentium velit, ipsam, inventore quaerat sed iste provident nihil
+        ipsa veritatis perspiciatis corrupti. Eaque reprehenderit unde autem
+        fugiat totam assumenda! Est.
+      </h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/navbar/Nabvar.jsx
@@ -0,0 +1,78 @@
+import React, { useEffect, useState } from 'react';
+import './Nabvar.scss';
+
+const Nabvar = () => {
+  const [active, setActive] = useState(false);
+  const [open, setOpen] = useState(false);
+
+  const isActive = () => {
+    window.scrollY > 0 ? setActive(true) : setActive(false);
+  };
+  useEffect(() => {
+    window.addEventListener('scroll', isActive);
+    return () => {
+      window.removeEventListener('scroll', isActive);
+    };
+  }, []);
+
+  const currentUser = {
+    id: 1,
+    username: 'Jhon Doe',
+    isSeller: true,
+  };
+
+  return (
+    <div className={active ? 'navbar active' : 'navbar'}>
+      <div className="container">
+        <div className="logo">
+          <span className="text">Fiverr</span>
+          <span className="dot">.</span>
+        </div>
+        <div className="links">
+          <span>Liverr Business</span>
+          <span>Explore</span>
+          <span>English</span>
+          <span>Sign in</span>
+          {!currentUser?.isSeller && <span>Become a Seller</span>}
+          {!currentUser && <button>Join</button>}
+          {currentUser && (
+            <div className="user" onClick={() => setOpen(!open)}>
+              <img
+                src="https://images.pexels.com/photos/1115697/pexels-photo-1115697.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+              <span>{currentUser?.username}</span>
+
+              {open && (
+                <div className="options">
+                  {currentUser?.isSeller && (
+                    <>
+                      <span>Gigs</span>
+                      <span>Add New Gig</span>
+                    </>
+                  )}
+
+                  <span>Orders</span>
+                  <span>Message</span>
+                  <span>Logout</span>
+                </div>
+              )}
+            </div>
+          )}
+        </div>
+      </div>
+
+      {active && (
+        <>
+          <hr />
+          <div className="menu">
+            <span>Test</span>
+            <span>Test2</span>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Nabvar;
