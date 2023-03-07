87c8af6b29c66e89f05354b588743c4a72555328
Gabriel Aristizabal <gabrielhotm2020@outlook.com>
2023-03-07 18:20:47 -0500

    5.Category Slide

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
 
 import Featured from '../../components/featured/Featured';
+import Slide from '../../components/slide/Slide';
 import TrustedBy from '../../components/trustedby/TrustedBy';
 import './Home.scss';
+
+import { cards } from '../../data';
+import CategoryCard from '../../components/categorycard/CategoryCard';
 
 const Home = () => {
   return (
     <div className="home">
       <Featured />
       <TrustedBy />
+      <Slide slidesToShow={5} arrowsScroll={5}>
+        {cards.map((card) => (
+          <CategoryCard key={card.id} item={card} />
+        ))}
+      </Slide>
     </div>
   );
 };
